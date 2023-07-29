ae18ecdded065386fdcb3feb355892552646a13c
Siddhant Gunjal <Siddhant.Gunjal@asl.mum.sst>
2023-07-29 20:19:51 +0530

    Commit Assignments

---
+++ 45-Number-Guesser-Assignment-Example/src/com/aurinpro/model/NumberGuess.java
@@ -0,0 +1,57 @@
+package com.aurinpro.model;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuess {
+
+	Scanner sc = new Scanner(System.in);
+	Random rm = new Random();
+
+	private int answer = rm.nextInt(11);
+	private int userInput;
+
+	public void setInput(int userInput) {
+		this.userInput = guessTheNumber(userInput);
+	}
+
+	public int getInput() {
+		return userInput;
+	}
+
+	public int getNumberls() {
+		return answer;
+	}
+
+	public int guessTheNumber(int userInput) {
+		for (int i = 1; i <= 6; i++) {
+			if (userInput != answer && userInput < answer) {
+				System.out.println("Sorry Your Guessed Number " + userInput + " is low. Re-Enter the Higher Number :");
+				userInput = sc.nextInt();
+			} else if (userInput != answer && userInput > answer) {
+				System.out.println("Sorry Your Guessed Number " + userInput + " is high. Re-Enter a lower Number :");
+				userInput = sc.nextInt();
+			} else {
+				System.out.println("Your Guessed Number is Right, The Number is : " + userInput
+						+ "\n The correct answer is : " + getNumberls());
+				continueGame();
+
+			}
+
+		}
+		return userInput;
+		
+	}
+
+	public void continueGame() {
+		System.out.println("Do you want to continue playing? (yes/no):");
+		String choice = sc.next();
+		if (choice.equalsIgnoreCase("yes")) {
+			System.out.println("Game is Restarted : ");
+			guessTheNumber(userInput);
+		} else {
+			System.out.println("Thank you for playing!!");
+		}
+	}
+
+}
